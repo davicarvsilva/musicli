2852433777ac59a46c3e8fde2f6973f1d56ed916
davicarvsilva <davicarv13@gmail.com>
2022-06-21 20:16:25 -0300

    feat:set up 'music' app

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('core.urls', namespace='core')),
     path('admin/', admin.site.urls),
-    path('', include('core.urls', namespace='core'))
+    path('music/', include('music.urls', namespace='music'))
 ]
