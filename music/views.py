85e8c34a1f8ac442081b98c2a01c06653bdd2549
davicarvsilva <davicarv13@gmail.com>
2022-06-27 08:07:53 -0300

    fix: apply restriction to userto song forms, only audio files being accepted

--- music/views.py
+++ music/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.views import View
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import redirect
-
+import magic
+from django.forms.utils import ErrorList
 
 from music.models import Song
 from .forms import SongForm
@@ -30,6 +31,11 @@ class SongFormView(View):
     
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST, request.FILES, user=request.user)
+        f = magic.Magic() 
+
+        if "Audio file" not in f.from_buffer(request.FILES['file'].read()):
+            form.add_error('file', 'Enter a audio file')
+
         if request.method == 'POST' and request.FILES['file']:
             if form.is_valid():
                 form.save()
